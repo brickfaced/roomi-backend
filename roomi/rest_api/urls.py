ad2dd6c0d67d3316de95268cc6770adb1e61ee9e
brickfaced <brickfazed@hotmail.com>
2018-06-05 14:15:02 -0700

    Added a user-detail route to get user information and created an ansible playbook for faster deployment to ec2 instance

--- roomi/rest_api/urls.py
+++ roomi/rest_api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from rest_framework.authtoken import views
+from .views import UserApi
 
 urlpatterns = [
-    path('auth-token-login/', views.obtain_auth_token),
+    path('login/', views.obtain_auth_token),
+    path('user/', UserApi.as_view(), name='user-detail')
 ]
--- roomi/roomi/settings.py
+++ roomi/roomi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '21j2p9!o^3a@zgy&x58qa+eu+d2b^j32*2bz_+4!vx(6ycrhz='
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
